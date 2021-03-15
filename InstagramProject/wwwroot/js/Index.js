a44df4ed48a90d1d25a8111a390fc05a16c76d92
David <inkudave@gmail.com>
2021-03-15 00:35:59 -0600

    This is my 7th Assignment

--- InstagramProject/wwwroot/js/Index.js
+++ InstagramProject/wwwroot/js/Index.js
@@ -16,155 +16,40 @@ div.appendChild(signUpButton);
 var lineBreak =
     document.createElement("br");
 div.appendChild(lineBreak);
-var image1Span =
-    document.createElement("span");
-image1Span.innerHTML = "Image 1 ";
-div.appendChild(image1Span);
-var image2Span =
-    document.createElement("span");
-image2Span.innerHTML = "Image 2 ";
-div.appendChild(image2Span);
-var image3Span =
-    document.createElement("span");
-image3Span.innerHTML = "Image 3 ";
-div.appendChild(image3Span);
-var image4Span =
-    document.createElement("span");
-image4Span.innerHTML = "Image 4 ";
-div.appendChild(image4Span);
-var image5Span =
-    document.createElement("span");
-image5Span.innerHTML = "Image 5 ";
-div.appendChild(image5Span);
-var lineBreak2 =
-    document.createElement("br");
-div.appendChild(lineBreak2);
 
 
 
-var image6Span =
-    document.createElement("span");
-image6Span.innerHTML = "Image 6 ";
-div.appendChild(image6Span);
-var image7Span =
-    document.createElement("span");
-image7Span.innerHTML = "Image 7 ";
-div.appendChild(image7Span);
-var image8Span =
-    document.createElement("span");
-image8Span.innerHTML = "Image 8 ";
-div.appendChild(image8Span);
-var image9Span =
-    document.createElement("span");
-image9Span.innerHTML = "Image 9 ";
-div.appendChild(image9Span);
-var image10Span =
-    document.createElement("span");
-image10Span.innerHTML = "Image 10 ";
-div.appendChild(image10Span);
-var lineBreak4 =
-    document.createElement("br");
-div.appendChild(lineBreak4);
 
 
-
-var image11Span =
-    document.createElement("span");
-image11Span.innerHTML = "Image 11 ";
-div.appendChild(image11Span);
-var image12Span =
-    document.createElement("span");
-image12Span.innerHTML = "Image 12 ";
-div.appendChild(image12Span);
-var image13Span =
-    document.createElement("span");
-image13Span.innerHTML = "Image 13 ";
-div.appendChild(image13Span);
-var image14Span =
-    document.createElement("span");
-image14Span.innerHTML = "Image 14 ";
-div.appendChild(image14Span);
-var image20Span =
-    document.createElement("span");
-image20Span.innerHTML = "Image 20 ";
-div.appendChild(image20Span);
-
-var lineBreak5 =
-    document.createElement("br");
-div.appendChild(lineBreak5);
+var table = document.createElement("table")
+var tbody = document.createElement("tbody")
+var trow = null;
+var td = null;
+var imageSpan = null;
+var counter = 0; 
 
 
+for (var i = 1; i <= 25; i++) {
+    if (counter == 5) {
+        tbody.appendChild(trow);
+        trow = document.createElement("tr");
+        counter = 0;
+    }
 
-var image21Span =
-    document.createElement("span");
-image21Span.innerHTML = "Image 21 ";
-div.appendChild(image21Span);
-var image22Span =
-    document.createElement("span");
-image22Span.innerHTML = "Image 22 ";
-div.appendChild(image22Span);
-var image23Span =
-    document.createElement("span");
-image23Span.innerHTML = "Image 23 ";
-div.appendChild(image23Span);
-var image24Span =
-    document.createElement("span");
-image24Span.innerHTML = "Image 24 ";
-div.appendChild(image24Span);
-var image25Span =
-    document.createElement("span");
-image25Span.innerHTML = "Image 25 ";
-div.appendChild(image25Span);
+    td = document.createElement("td");
+    imageSpan = document.createElement("span");
+    imageSpan.innerHTML = "Image " + i;
 
-var lineBreak3 =
-    document.createElement("br");
-div.appendChild(lineBreak3);
-
-
-var lineBreak5 =
-    document.createElement("br");
-div.appendChild(lineBreak5);
-
-
-var aboutSpan =
-    document.createElement("span");
-aboutSpan.innerHTML = "About ";
-div.appendChild(aboutSpan);
-var blogSpan =
-    document.createElement("span");
-blogSpan.innerHTML = "Blog ";
-div.appendChild(blogSpan);
-var jobSpan =
-    document.createElement("span");
-jobSpan.innerHTML = "Job ";
-div.appendChild(jobSpan);
-var helpSpan =
-    document.createElement("span");
-helpSpan.innerHTML = "Help ";
-div.appendChild(helpSpan);
-var lineBreak3 =
-    document.createElement("br");
-div.appendChild(lineBreak3);
-var englishSpan =
-    document.createElement("span");
-englishSpan.innerHTML = "English ";
-div.appendChild(englishSpan);
-var yearSpan =
-    document.createElement("span");
-yearSpan.innerHTML = "2021";
-div.appendChild(yearSpan);
-var gramSpan =
-    document.createElement("span");
-gramSpan.innerHTML = "My Gram";
-div.appendChild(gramSpan);
-var myNumber = 10;
-var myString = "A string";
-var myBoolean = false;
-console.log(myNumber);
-console.log(myString);
-console.log(myBoolean);
-function SumOfNumbers(x, y) {
-    var result = x + y;
-    console.log(result);
-
+    td.appendChild(imageSpan);
+    trow.appendChild(td);
+    counter++;
 }
+    tbody.appendChild(trow);
+    table.appendChild(tbody);
+    div.appendChild(table);
+
+
+    var image1Span =
+        document.createElement("span");
+    image1Span.innerHTML = "Image 1 ";
+    div.appendChild(image1Span);
